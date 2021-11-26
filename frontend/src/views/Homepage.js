fd50231ac83f2d0895334f8d7aaad65effe889a8
Samuel Matthew <samuelmatthew211@gmail.com>
2021-11-26 22:13:39 +0700

    :sparkles: Add pagination

--- frontend/src/views/Homepage.js
+++ frontend/src/views/Homepage.js
@@ -6,8 +6,7 @@ import PokeCard from "../components/PokeCard";
 export default function HomePage() {
   const [pokemons, setPokemons] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-
-  // const [page, setPage] = useState(0);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     setIsLoading(true);
@@ -16,11 +15,18 @@ export default function HomePage() {
       url: "/pokemon",
       params: {
         limit: "20",
-        // offset: page * 12,
+        offset: (page - 1) * 12,
       },
     })
       .then(({ data }) => {
         setPokemons(data.results);
+        // const oldPokemons = pokemons;
+        // data.results.forEach((el) => {
+        //   oldPokemons.push(el);
+        // });
+
+        // console.log(data.results);
+        // setPokemons([...pokemons, data.results]);
       })
       .catch((err) => {
         console.log(err);
@@ -28,7 +34,12 @@ export default function HomePage() {
       .finally(() => {
         setIsLoading(false);
       });
-  }, []);
+  }, [page]);
+
+  const handleChangePage = (event) => {
+    console.log(event.target.innerHTML);
+    setPage(event.target.innerHTML);
+  };
 
   if (isLoading) {
     return (
@@ -46,7 +57,52 @@ export default function HomePage() {
     <div id="HomePage" className="page py-5">
       <div className="wrapper">
         <h1 className="text-center p-3 mb-5">Pokédex</h1>
-
+        <nav aria-label="Page navigation example">
+          <ul className="pagination">
+            {/* <li className="page-item">
+              <a className="page-link" href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+                <span className="sr-only">Previous</span>
+              </a>
+            </li> */}
+            <li className="page-item">
+              <a
+                className="page-link"
+                href="#"
+                onClick={handleChangePage}
+                value={"1"}
+              >
+                1
+              </a>
+            </li>
+            <li className="page-item">
+              <a
+                className="page-link"
+                href="#"
+                onClick={handleChangePage}
+                value={"2"}
+              >
+                2
+              </a>
+            </li>
+            <li className="page-item">
+              <a
+                className="page-link"
+                href="#"
+                onClick={handleChangePage}
+                value={"3"}
+              >
+                3
+              </a>
+            </li>
+            {/* <li class="page-item">
+              <a class="page-link" href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+                <span class="sr-only">Next</span>
+              </a>
+            </li> */}
+          </ul>
+        </nav>
         <div className="cardContainer d-flex flex-row flex-wrap mx-3">
           {pokemons.map((el) => {
             return <PokeCard details={el.name} key={el.name} />;
